bff01ba06581abf2dfcb7a266167b04104088e45
penguin-yeh <77048324+penguin-yeh@users.noreply.github.com>
2022-05-25 00:44:48 +0800

    Update problem1_6.py

--- problem1_6.py
+++ problem1_6.py
@@ -1,1 +1,18 @@
+#{ key1:value1, key2,value2 }: dict
+data_transforms = {
+        'train': transforms.Compose([ #Compose: 將transform組合在一起
+                                     
+            transforms.Resize((224,224)), #(H, W):將圖片轉成224*224
+          
+            transforms.ColorJitter(brightness=0, contrast=0, saturation=0.2),
+          
+            transforms.ToTensor(), #將PIL.image/numpy.ndarry轉成torch.FloadTensor,並normalize到[0.0, 1.0]
 
+        ]),
+        'val': transforms.Compose([
+          
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+          
+        ]),
+    }
