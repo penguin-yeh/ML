4e82754212c7bac7abec4f36b972b4f0d780bac9
penguin-yeh <77048324+penguin-yeh@users.noreply.github.com>
2022-05-25 00:47:24 +0800

    Create problem1_5.py

---
+++ problem1_5.py
@@ -0,0 +1,16 @@
+#{ key1:value1, key2,value2 }: dict
+data_transforms = {
+        'train': transforms.Compose([ #Compose: 將transform組合在一起
+                                     
+            transforms.Resize((224,224)), #(H, W):將圖片轉成224*224
+
+            transforms.RandomRotation(90), 
+          
+            transforms.ToTensor(), #將PIL.image/numpy.ndarry轉成torch.FloadTensor,並normalize到[0.0, 1.0]
+
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+        ]),
+    }
